b978cc528adb41864e9835e96deb856a87432774
Paxyutin <91972249+Wault-Tec@users.noreply.github.com>
2022-12-14 17:28:43 +0500

    some change

--- src/api/firebaseApi.ts
+++ src/api/firebaseApi.ts
@@ -1,20 +1,27 @@
 import { firestore } from "../firebase";
 import { collection, doc, getDocs, addDoc, deleteDoc, updateDoc  } from "firebase/firestore";
+import { Data } from "src/type";
 import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject  } from "firebase/storage";
 
 export const getRequest = async () => {
     try {
-        //  @ts-ignore
-        let data = [];
+        let data: Data[] = [];
         const querySnapshot = await getDocs(collection(firestore, "Documents"));
         querySnapshot.forEach((doc) => {
-            //  @ts-ignore
             data = [...data, ...doc.data().data]
         });
-        //  @ts-ignore
         return data
-    } catch (e) {
-        //  @ts-ignore
+    } catch (e: any) {
+        throw new Error(e)
+    }   
+}
+
+
+export const deleteRequest = async (id: keyof Data) => {
+    try {
+        const docRef = doc(firestore, "Documents", "documents1")
+        await deleteDoc(docRef);
+    } catch (e: any) {
         throw new Error(e)
     }   
 }
